8e9a685a2a858124e8434e774c175b1c403fec5b
Jeff Feige <jfeige@gmail.com>
2018-10-04 22:36:07 -0700

    back to "normal" autorandr 1.7

--- monitor-layout-chooser/monitor-layout-chooser-init.sh
+++ monitor-layout-chooser/monitor-layout-chooser-init.sh
@@ -4,7 +4,7 @@ chown user:users /custom/monitor-layout-chooser/autorandr-config
 ln -sf /custom/monitor-layout-chooser/autorandr-config /userhome/.config/autorandr
 cp /custom/monitor-layout-chooser/autorandr.py /usr/bin/autorandr
 cp /custom/monitor-layout-chooser/autorandr.service /lib/systemd/system
-systemctl enable autorandr.service ; \ 
-systemctl start autorandr.service ; \ 
-cp /custom/monitor-layout-chooser/40-monitor-hotplug.rules /lib/udev/rules.d ; \ 
-udevadm control --reload &
+systemctl enable autorandr.service  
+# systemctl start autorandr.service 
+cp /custom/monitor-layout-chooser/40-monitor-hotplug.rules /lib/udev/rules.d 
+udevadm control --reload
