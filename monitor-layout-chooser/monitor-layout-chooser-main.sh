8768b0e0dd250fcdcdcd9cfa424ddb0b01abed13
Jeff Feige <jfeige@gmail.com>
2018-10-07 09:53:54 -0700

    added delete all layouts

--- monitor-layout-chooser/monitor-layout-chooser-main.sh
+++ monitor-layout-chooser/monitor-layout-chooser-main.sh
@@ -24,11 +24,12 @@ FALSE "Create layout" "CREA" \
 FALSE "Save layout" "SAVE" \
 FALSE "Show available layouts" "SHOW" \
 FALSE "Delete layout" "DELE" )
+FALSE "Delete all layouts" "DELA" )
 echo $mylayout
 
 case $mylayout in
 LOAD)	load-monitor-layout
-	;;
+;;
 CREA)	create-monitor-layout
 	;;
 SAVE)	save-monitor-layout
@@ -36,6 +37,8 @@ SAVE)	save-monitor-layout
 SHOW)	show-monitor-layout
 	;;
 DELE)	delete-monitor-layout
+	;;
+DELA)	delete-all-layouts
 	;;
 *) exit 0
    ;;
@@ -78,6 +81,10 @@ cd /userhome/.config/autorandr
 layout=(*/)
 mylayout=$(zenity --list --height $(( 70 * ${#layout[@]} )) --title="Delete layout" --column="Layout"  "${layout[@]%%/}")
 /${CUSTOMDIR}/${PROGRAMDIR}/autorandr.py --remove "${mylayout}"
+}
+
+delete-all-layouts () {
+rm -rf /userhome/.config/autorandr/*
 }
 
 main
